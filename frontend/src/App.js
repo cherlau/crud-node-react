4c1df50779c186310f52d1538ef2e4091aca2d77
cherlau <cherlau.prado@appfacilita.com>
2025-03-26 15:52:10 -0300

    created user form

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,9 +1,31 @@
+import Global from "./styles/global";
+import { toast, ToastContainer } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
+import styled from "styled-components";
+import Form from "./components/Form";
+
+const Container = styled.div`
+  width: 100%;
+  max-width: 800px;
+  margin-top: 20px;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  gap: 10px;
+`
+
+const Title = styled.h2``
 
 function App() {
   return (
-    <div className="App">
-
-    </div>
+    <>
+      <Container>
+        <Title>Usuários</Title>
+        <Form />
+      </Container>
+      <ToastContainer autoClose={3000} position="bottom-left" />
+      <Global />
+    </>
   );
 }
 
