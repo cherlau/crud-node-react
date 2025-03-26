6a8ac3f57e5612369f10c545fb38c1fb8c36ea07
cherlau <cherlau.prado@appfacilita.com>
2025-03-26 17:19:03 -0300

    api complete

--- api/controllers/user.js
+++ api/controllers/user.js
@@ -1,16 +1,66 @@
-import { db } from "../db.js";
+import {
+  db
+} from "../db.js";
 
 export const getUsers = (_, res) => {
-  const q = "SELECT * FROM usuarios"
+  const q = "SELECT * FROM usuarios";
 
-  console.log("Função getUsers foi chamada!")
+  console.log("Função getUsers foi chamada!");
 
   db.query(q, (err, data) => {
     if (err) {
-      console.error("Erro na consulta:", err)
-      return res.status(500).json({ error: "Erro na consulta" })
+      console.error("Erro na consulta:", err);
+      return res.status(500).json({
+        error: "Erro na consulta"
+      });
     }
 
-    return res.status(200).json(data)
-  })
-}
+    return res.status(200).json(data);
+  });
+};
+
+export const addUser = (req, res) => {
+  const q =
+    "INSERT INTO usuarios(`nome`, `email`, `fone`, `data_nascimento`) VALUES(?)";
+
+  const values = [
+    req.body.nome,
+    req.body.email,
+    req.body.fone,
+    req.body.data_nascimento,
+  ];
+
+  db.query(q, [values], (err) => {
+    if (err) return res.json(err);
+
+    return res.status(200).json("Usuário criado com sucesso.");
+  });
+};
+
+export const updateUser = (req, res) => {
+  const q =
+    "UPDATE usuarios SET `nome` = ?, `email` = ?, `fone` = ?, `data_nascimento` = ? WHERE `id` = ?";
+
+    const values = [
+      req.body.nome,
+      req.body.email,
+      req.body.fone,
+      req.body.data_nascimento,
+    ];
+
+    db.query(q, [...values, req.params.id], (err) => {
+      if (err) return res.json(err);
+
+      return res.status(200).json("Usuário atualizado com sucesso.");
+    })
+  }
+
+  export const deleteUser = (req, res) => {
+    const q = "DELETE FROM usuarios WHERE `id` = ?";
+
+    db.query(q, [req.params.id], (err) => {
+      if (err) return res.json(err);
+
+      return res.status(200).json("Usuário deletado com sucesso.");
+    })
+  }
