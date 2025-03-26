192ed7ec0c7f489dbb629e41b021b7eae598f515
cherlau <cherlau.prado@appfacilita.com>
2025-03-26 17:11:34 -0300

    feat: created grid componente, and get users from API

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,8 +1,11 @@
 import Global from "./styles/global";
+import { useState, useEffect } from "react";
 import { toast, ToastContainer } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
 import styled from "styled-components";
 import Form from "./components/Form";
+import Grid from "./components/Grid";
+import axios from "axios";
 
 const Container = styled.div`
   width: 100%;
@@ -17,11 +20,28 @@ const Container = styled.div`
 const Title = styled.h2``
 
 function App() {
+  const [users, setUsers] = useState([]);
+  const [onEdit, setOnEdit] = useState(null);
+
+  const getUsers = async () => {
+    try {
+      const res = await axios.get("http://localhost:8800");
+      setUsers(res.data.sort((a, b) => (a.nome > b.nome ? 1 : -1)));
+    } catch (error) {
+      toast.error(error);
+    }
+  };
+
+  useEffect(() => {
+    getUsers();
+  }, [setUsers])
+
   return (
     <>
       <Container>
         <Title>Usuários</Title>
         <Form />
+        <Grid users={users} />
       </Container>
       <ToastContainer autoClose={3000} position="bottom-left" />
       <Global />
